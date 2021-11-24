a531990d8a0a0ceff12a107d9e1f715637858b98
William Wang <sleep27075187@gmail.com>
2021-11-24 16:55:47 +0800

    deploy test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  target: 'static',
+  router: {
+    base: '/rail-searching/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'rail-nuxt',
