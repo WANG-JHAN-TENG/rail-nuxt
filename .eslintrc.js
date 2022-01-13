6bd28321bcf84577c5932cf1dbc29b3947ca5883
TAIDI\william.wang <sleep27075187@gmail.com>
2022-01-13 19:23:21 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'linebreak-style': 'off',
+    'no-tabs': 0,
+    'space-in-parens': ['error', 'always'],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+  },
+};
